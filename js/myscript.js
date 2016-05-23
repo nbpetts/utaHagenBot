804aa3fb8ba2ac7e120552a9363b71c26524d5cd
Nathan Petts <nbpetts@gmail.com>
2016-05-23 12:20:19 -0500

    added voice commands and now Uta replys to lines

--- js/myscript.js
+++ js/myscript.js
@@ -1,15 +1,93 @@
 "use strict";
+// caching replies
+var hello = new SpeechSynthesisUtterance("hello"),
+    popovaDidnITell = new SpeechSynthesisUtterance("Didn't you tell him that my husband is dead and that I see no one?"),
+    popovaAndISaid = new SpeechSynthesisUtterance("And I said I see no one!"),
+    popovaAllRight = new SpeechSynthesisUtterance("All right, all right, tell him all right. Reall! The nerve of some people!");
+
+function say (utterance) {
+  window.speechSynthesis.speak(utterance);
+};
+
+function startDictation() {
+    annyang.abort();
+    if (window.hasOwnProperty('webkitSpeechRecognition')) {
+
+        var recognition = new webkitSpeechRecognition();
+        var final_transcript = "";
+
+        recognition.continuous = true;
+        recognition.maxAlternatives = 5;
+        recognition.interimResults = true;
+
+
+
+
+        recognition.lang = "en-US";
+        recognition.start();
+      var recognition;
+
+        recognition.onresult = function(e) {
+            var interim_transcript = '';
+            if (typeof(e.results) == 'undefined') {
+                reset();
+                return;
+            }
+            for (var i = e.resultIndex; i < e.results.length; ++i) {
+                var val = e.results[i][0].transcript;
+                if (e.results[i].isFinal) {
+                    final_transcript += " " + val;
+                } else {
+                    interim_transcript += " " + val;
+                }
+            }
+            document.getElementById("final").innerHTML = final_transcript;
+            document.getElementById("notFinal").innerHTML = interim_transcript;
+        };
+
+        recognition.onerror = function(e) {
+            var msg = e.error + " error";
+            if (e.error === 'no-speech') {
+                msg =
+                    "No speech was detected. Please turn on the microphone and try again.";
+            } else if (e.error === 'audio-capture') {
+                msg =
+                    "Please ensure that your microphone is connected to the computer and turned on.";
+            } else if (e.error === 'not-allowed') {
+                msg =
+                    "Dication.io cannot access your microphone. Please go to chrome://settings/contentExceptions#media-stream and allow Microphone access to this website.";
+            }
+            document.getElementById("error").innerHTML = "<p>" + msg + "</p>";
+            setTimeout(function() {
+                document.getElementById("error").innerHTML = "";
+            }, 5000);
+        };
+
+    } // end hasOwnProperty
+    annyang.start();
+};// end start dictiation
 
 if (annyang) {
     // Add our commands to annyang
+
+
     annyang.addCommands({
         'hello': function() {
-            // window.speechSynthesis.speak(new SpeechSynthesisUtterance("hello"));
-            console.log('Hello world!');
+            say(hello);
+
         },
 
-        'or in rain': function() {
-            // window.speechSynthesis.speak(new SpeechSynthesisUtterance("When the hurlyburlys done"));
+        "*can't wait": function() {
+            say(popovaDidnITell);
+        },
+        "*very important": function() {
+            say(popovaAndISaid);
+        },
+        "*right now": function() {
+            say(popovaAllRight);
+        },
+        "*rehearse": function() {
+            startDictation();
         }
 
 
@@ -17,7 +95,7 @@ if (annyang) {
 
 
     // Start listening.
-    // annyang.start();
+    annyang.start();
 
 
 
@@ -30,65 +108,3 @@ if (annyang) {
     // // Render KITT's interface
     // SpeechKITT.vroom();
 }
-
-$(function() {
-
-    $("#toggleRecord").click(startDictation);
-
-    function startDictation() {
-
-        if (window.hasOwnProperty('webkitSpeechRecognition')) {
-
-            var recognition = new webkitSpeechRecognition();
-            var final_transcript = "";
-
-            recognition.continuous = true;
-            recognition.maxAlternatives = 5;
-            recognition.interimResults = true;
-
-
-
-
-            recognition.lang = "en-US";
-            recognition.start();
-
-            recognition.onresult = function(e) {
-                var interim_transcript = '';
-                if (typeof(e.results) == 'undefined') {
-                    reset();
-                    return;
-                }
-                for (var i = e.resultIndex; i < e.results.length; ++i) {
-                    var val = e.results[i][0].transcript;
-                    if (e.results[i].isFinal) {
-                        final_transcript += " " + val;
-                    } else {
-                        interim_transcript += " " + val;
-                    }
-                }
-                document.getElementById("final").innerHTML = final_transcript;
-                document.getElementById("notFinal").innerHTML = interim_transcript;
-            };
-
-            recognition.onerror = function(e) {
-                var msg = e.error + " error";
-                if (e.error === 'no-speech') {
-                    msg =
-                        "No speech was detected. Please turn on the microphone and try again.";
-                } else if (e.error === 'audio-capture') {
-                    msg =
-                        "Please ensure that your microphone is connected to the computer and turned on.";
-                } else if (e.error === 'not-allowed') {
-                    msg =
-                        "Dication.io cannot access your microphone. Please go to chrome://settings/contentExceptions#media-stream and allow Microphone access to this website.";
-                }
-                document.getElementById("error").innerHTML = "<p>" + msg + "</p>";
-                setTimeout(function() {
-                    document.getElementById("error").innerHTML = "";
-                }, 5000);
-            };
-
-        } // end hasOwnProperty
-    };
-
-}); // end shorhand doc. ready
